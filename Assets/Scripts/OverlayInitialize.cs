44989465436a1f29ba669b1f848f15aa9a7068bc
meronmks <meronmks@users.noreply.github.com>
2021-04-07 20:15:45 +0900

    Overlayの初期化処理追加

---
+++ Assets/Scripts/OverlayInitialize.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlayInitialize : MonoBehaviour
+{
+    [SerializeField] private GameObject _OverlaySystem = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var args = System.Environment.GetCommandLineArgs();
+        if (args.Length > 1)
+        {
+            if (args[1].Equals("/vr"))
+            {
+                _OverlaySystem.SetActive(true);
+            }
+        }
+    }
+}
